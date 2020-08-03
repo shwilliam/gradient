3c48f44890ebc6d40fdd090ea716c50ced816524
William Lindvall <shwilliam@hey.com>
2020-08-02 20:14:20 -0700

    update color calc logic

--- src/lib/colors.js
+++ src/lib/colors.js
@@ -7,16 +7,14 @@ export const brightenIfDark = color =>
 export const generateColorsFromHash = hash => {
   const colorA = brightenIfDark(
     new Color({
-      h: minModulus(360, 20, hash),
-      s: minModulus(100, 30, hash),
-      l: minModulus(100, 40, hash) - 20,
+      h: minModulus(360, 0, hash * 42),
+      s: minModulus(100, 0, hash),
+      l: minModulus(100, 60, hash) - 30,
     })
-      .saturate(0.5)
-      .lighten(0.3),
+      .saturate(0.8)
+      .lighten(0.2),
   )
-  const colorB = brightenIfDark(
-    colorA.rotate(minModulus(180, 90, hash) + 90),
-  ).saturate(0.5)
+  const colorB = brightenIfDark(colorA.rotate(180)).saturate(0.8)
 
   const hslA = colorA.hsl().string()
   const hslB = colorB.hsl().string()
